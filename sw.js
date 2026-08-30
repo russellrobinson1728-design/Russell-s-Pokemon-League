0988e72435a02cc62e6d62bbed3d6f8b7d56bc18
russellrobinson1728-design <russellrobinson1728@gmail.com>
2026-08-31 04:10:40 +0530

    Initial app

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE="russ-pokemon-league-v1";
+const ASSETS=["./","./index.html","./manifest.json"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{
+    const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;
+  }).catch(()=>caches.match("./index.html"))));
+});
